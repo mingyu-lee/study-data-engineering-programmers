58ceccbecaa7e87f8582efe6cafe443f1afe7301
Mingyu Lee <leemingyu05@gmail.com>
2020-07-29 00:03:50 +0900

    ACTIVE_USER 알리아스 추가

--- assignment/week2/mau_count.sql
+++ assignment/week2/mau_count.sql
@@ -1,7 +1,7 @@
 -- 월별 MAU Counting
 select
     to_char(ST.TS, 'YYYY-MM') as YEAR_MONTH
-  , count(distinct (USC.USERID))
+  , count(distinct(USC.USERID)) as ACTIVE_USER
 from RAW_DATA.USER_SESSION_CHANNEL         USC
      inner join RAW_DATA.SESSION_TIMESTAMP ST on USC.SESSIONID = ST.SESSIONID
 group by 1
